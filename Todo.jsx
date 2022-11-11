e3dd9ee3f75fdda3d6f83462806d7b40ad298776
Chris <moltencore95@hotmail.de>
2022-11-11 17:53:31 +0100

    react crashcourse

---
+++ Todo.jsx
@@ -0,0 +1,19 @@
+import './Todo.css'
+
+function Todo({ title }){
+    function deleteTodo(id) {
+        console.log('deleteTodo()', title)
+    }
+
+
+
+    return(
+<div className="todo">
+        <p>{ title }</p>
+        <button onClick={() => deleteTodo(1)}
+        >Delete</button>
+        </div>
+    )
+}
+
+export default Todo
